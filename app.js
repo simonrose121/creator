ded28a3cdcc234d44eda0af5020fe5df26530f61
Simon Rose <simonrose121@gmail.com>
2015-08-01 12:25:11 +0100

    App.js and database config

---
+++ app.js
@@ -0,0 +1,27 @@
+// includes
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// config files
+var db = require('./config/db');
+var port = process.env.PORT || 8080;
+
+// connect to database
+mongoose.connect(db.url);
+
+// get POST parameter database
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json '}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(express.static(__dirname + '/public'));
+
+// routes
+require('./app/routes')(app);
+
+app.listen(port);
+
+// expose app
+exports = module.exports = app;
