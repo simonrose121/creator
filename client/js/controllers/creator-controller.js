8a30bb37d9757b1c777f5c3e83a4fe74f7d58d77
Simon Rose <simonrose121@gmail.com>
2015-08-11 19:10:59 +0100

    Added angular-resource and basic instructions on front end

--- client/js/controllers/creator-controller.js
+++ client/js/controllers/creator-controller.js
@@ -1,3 +1,9 @@
 app.controller('creator-controller', ['$scope', '$http', function($scope, $http) {
-
+  $scope.instructions = [];
+  $scope.submit = function() {
+    if ($scope.text) {
+      $scope.instructions.push(this.text);
+      $scope.text = '';
+    }
+  };
 }]);
