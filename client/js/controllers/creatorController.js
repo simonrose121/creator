67d954935e0ed8456c0a571d5a3d594d58ee8275
Simon Rose <simonrose121@gmail.com>
2015-09-04 16:24:02 +0100

    Aesthetic changes, added default sprite and background and some other styling

--- client/js/controllers/creatorController.js
+++ client/js/controllers/creatorController.js
@@ -1,7 +1,7 @@
 app.controller('creatorController', ['$scope', '$http', 'languageService', function($scope, $http, languageService) {
   $scope.instructions = [];
-  // $scope.instructions.push("move up 10");
-  // $scope.instructions.push("move left 8");
+  $scope.instructions.push("move up 10");
+  $scope.instructions.push("move left 8");
   $scope.submit = function() {
     if ($scope.text) {
       $scope.instructions.push(this.text);
